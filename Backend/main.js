5c215165fa2e4750172bf3a67baf334d8ce05d67
Lucas Capart <lucas.capart80@gmail.com>
2023-03-21 17:12:29 +0100

    Création main.js + liaison HTML
    Création du main.js + déclaration dans le fichier HTML.

---
+++ Backend/main.js
@@ -0,0 +1,82 @@
+async function getProjects() {
+  try {
+    const response = await fetch('http://localhost:5678/api/works');
+    const projects = await response.json();
+    return projects;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function renderProjects(categoryId = '') {
+  const projects = await getProjects();
+  const categories = await getCategories();
+  const galleryElement = document.querySelector('.gallery');
+  galleryElement.innerHTML = '';
+
+  // Filtrer les projets en fonction de la catégorie sélectionnée
+  const filteredProjects = categoryId ? projects.filter(project => project.categoryId === categoryId) : projects;
+
+  // Ajouter chaque projet à la galerie
+  for (const project of filteredProjects) {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery-item');
+
+    const imgElement = document.createElement('img');
+    imgElement.src = project.imageUrl;
+    imgElement.alt = project.title;
+
+    const h3Element = document.createElement('h3');
+    h3Element.textContent = project.title;
+
+    galleryItem.appendChild(imgElement);
+    galleryItem.appendChild(h3Element);
+    galleryElement.appendChild(galleryItem);
+  }
+}
+
+async function getCategories() {
+  try {
+    const response = await fetch('http://localhost:5678/api/categories');
+    const categories = await response.json();
+    return categories;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Créer un ensemble de catégories disponibles
+const categorySet = new Set();
+
+// Ajouter un bouton "Tous"
+const allButton = document.createElement('button');
+allButton.textContent = 'Tous';
+allButton.addEventListener('click', () => renderProjects());
+categorySet.add(allButton);
+
+// Créer des boutons pour chaque catégorie
+async function createCategoryButtons() {
+  const categories = await getCategories();
+  for (const category of categories) {
+    const button = document.createElement('button');
+    button.textContent = category.name;
+    button.addEventListener('click', () => renderProjects(category.id));
+    categorySet.add(button);
+  }
+}
+
+// Ajouter les boutons à la page
+async function addButtonsToPage() {
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('button-container');
+  await createCategoryButtons();
+  buttonContainer.append(...categorySet);
+  const galleryElement = document.querySelector('.gallery');
+  galleryElement.before(buttonContainer);
+}
+
+// Appeler la fonction pour ajouter les boutons à la page
+addButtonsToPage();
+
+// Afficher tous les projets par défaut
+renderProjects();
