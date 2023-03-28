ad8dbf4de04f9cede9c39b5720426d9260b070dd
Lucas Capart <lucas.capart80@gmail.com>
2023-03-28 16:56:35 +0200

    Update CSS + HTML + connexion.js
    Ajout d'une marge pour .js-modal.
    
    Suppression du bouton "modifier" situé sous l'image de l'architecte.
    
    Modification en conséquence du fichier connexion.js

--- FrontEnd/verif_connexion.js
+++ FrontEnd/verif_connexion.js
@@ -2,8 +2,7 @@
 const loggedInBar = document.querySelector('.logged-in');
 const filterBar = document.querySelector('.button-container'); //Déclare la variable filterBar sans lui affecter de valeur pour l'instant
 const loginLink = document.querySelector('#login-link');
-const modifButton = document.querySelector('.header_projects button');
-const modifButton2 = document.querySelector('.button-image button');
+const modifButton = document.querySelector('.js-modal');
 
 // Fonction pour vérifier si l'utilisateur est connecté ou non
 function checkIfUserIsLoggedIn() {  
@@ -13,7 +12,6 @@ function checkIfUserIsLoggedIn() {
     loginLink.innerHTML = '<li>logout</li>'; // Modification du texte du bouton de connexion pour afficher "logout"
     filterBar.style.display='none';
     modifButton.style.display='flex';
-    modifButton2.style.display='flex';
     }
      else {
     loggedInBar.style.display = 'none'; // Masquage de la barre de navigation correspondante
@@ -22,7 +20,6 @@ function checkIfUserIsLoggedIn() {
     loginLink.innerHTML = '<li>login</li>'; // Modification du texte du bouton de connexion pour afficher "login"
     filterBar.style.display='flex';
     modifButton.style.display='none';
-    modifButton2.style.display='none';
   }
 }
 // Fonction pour gérer la déconnexion de l'utilisateur
